81d618f4cd34037f74bf2d8c76d64457e4f43111
wcodewiz <samuelclinton975@gmail.com>
2024-02-13 00:35:40 -0800

    expose x, change by wcodewiz

---
+++ src/components/x/index.ts
@@ -0,0 +1,57 @@
+import Accordion from './Accordion';
+import Alert from './Alert';
+import { Spin } from './Animation';
+import { Bounce } from './Animation';
+import { Ping } from './Animation';
+import { Pulse } from './Animation';
+import Badge from './Badge';
+import BottomNavigator from './BottomNavigator';
+import BreadCrumbs from './BreadCrumb';
+import Card from './Card';
+import Chip from './Chip';
+import Choice from './Choice';
+import ConfirmBox from './ConfirmBox';
+import Dialog from './Dialog';
+import DropDown from './Dropdown';
+import Fade from './Fade';
+import { List } from './List';
+import ListTile from './ListTile';
+import ProgressBar from './ProgressBar';
+import Rating from './Rating';
+import Slider from './Slider';
+import Snackbar from './snackbar';
+import Steps from './Steps';
+import Tab from './Tab';
+import Timeline from './TimeLine';
+import ToolTip from './Tooltip';
+import Zoom from './Zoom';
+
+export {
+    Accordion,
+    Alert,
+    Spin,
+    Bounce,
+    Ping,
+    Pulse,
+    Badge,
+    BottomNavigator,
+    Card,
+    BreadCrumbs,
+    Chip,
+    Choice,
+    ConfirmBox,
+    Dialog,
+    DropDown,
+    Fade,
+    ListTile,
+    List,
+    ProgressBar,
+    Rating,
+    Slider,
+    Snackbar,
+    Steps,
+    Tab,
+    Timeline,
+    ToolTip,
+    Zoom
+};
